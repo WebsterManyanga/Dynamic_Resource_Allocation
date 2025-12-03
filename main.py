a83bb27079ab6d0bbcf38e6a7ad12f3cc07aef2f
Webster Manyanga <strategywebstermanyanga@gmail.com>
2025-12-03 06:33:58 +0100

    created ResourcePool class

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ class Process:
         self.cpu_time = cpu_time
 
 
+class ResourcePool:
+    def __init__(self, total_memory):
+        self.total_memory = total_memory
+
+        self.allocated_memory = 0
+
+    def allocate(self, process):
+        self.allocated_memory += process.required_memory
+
+    def release(self, process):
+        self.allocated_memory -= process.required_memory
+
